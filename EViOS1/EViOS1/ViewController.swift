a58978cf8996073e80a0f9d81c5808ef2ad14c34
Valérian Paillet <paillet.valerian@gmail.com>
2023-06-30 10:10:26 +0200

    add exo2

--- EViOS1/EViOS1/ViewController.swift
+++ EViOS1/EViOS1/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var userIcon: UIImageView!
+    @IBOutlet weak var viewTap: UIView!
+    @IBOutlet weak var imageInView: UIImageView!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginField: UITextField!
     
-
+    var isHidden: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -19,9 +24,34 @@ class ViewController: UIViewController {
         //rendre l'image ronde
         userIcon.layer.cornerRadius = userIcon.frame.size.height/2
         userIcon.clipsToBounds = true
-
         
+        //rendre la vue cliquable
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
+        viewTap.addGestureRecognizer(tapGesture)
+        
+        //gestion du clavier
+        loginField.keyboardType = .default
+        loginField.textContentType = .username
+        passwordField.textContentType = .password
+        
+        //fermeture du clavier avec un tap
+        let tapOnView = UITapGestureRecognizer(target: view, action:
+        #selector(UIView.endEditing))
+        view.addGestureRecognizer(tapOnView)
 
+    }
+    
+    @objc func viewTapped(){
+        if(isHidden){
+            imageInView.image = UIImage(named: "eye_on_icon")
+            passwordField.isSecureTextEntry = false
+            isHidden = false
+        } else {
+            imageInView.image = UIImage(named: "eye_off_icon")
+            passwordField.isSecureTextEntry = true
+            isHidden = true
+        }
+    
     }
     
 
